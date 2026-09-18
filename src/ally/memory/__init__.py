d104c78c5183f5286d621fa6dc706272d008b52d
david00andersen <david00andersen@proton.me>
2026-09-18 16:42:43 -0700

    feat: export memory choice constants

--- src/ally/memory/__init__.py
+++ src/ally/memory/__init__.py
@@ -1,6 +1,8 @@
 """Long-term memory domain models and contracts."""
 
 from ally.memory.models import (
+    MEMORY_KINDS,
+    MEMORY_PRIVACY_LEVELS,
     MemoryKind,
     MemoryPrivacy,
     MemoryRecord,
@@ -11,6 +13,8 @@ from ally.memory.models import (
 from ally.memory.store import MemoryStore
 
 __all__ = [
+    "MEMORY_KINDS",
+    "MEMORY_PRIVACY_LEVELS",
     "MemoryKind",
     "MemoryPrivacy",
     "MemoryRecord",
