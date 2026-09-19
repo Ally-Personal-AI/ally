3df5c235e9f9a02e276590d65bb5e2a2091cb2c2
david00andersen <david00andersen@proton.me>
2026-09-18 19:50:51 -0700

    feat: export memory proposal API

--- src/ally/memory/__init__.py
+++ src/ally/memory/__init__.py
@@ -3,6 +3,7 @@
 from ally.memory.models import (
     MEMORY_KINDS,
     MEMORY_PRIVACY_LEVELS,
+    MEMORY_SOURCE_TYPES,
     MemoryKind,
     MemoryPrivacy,
     MemoryRecord,
@@ -10,16 +11,27 @@ from ally.memory.models import (
     MemorySourceType,
     NewMemory,
 )
+from ally.memory.proposals import (
+    MemoryCandidate,
+    MemoryProposalBundle,
+    MemoryProposalError,
+    ModelMemoryProposer,
+)
 from ally.memory.store import MemoryStore
 
 __all__ = [
     "MEMORY_KINDS",
     "MEMORY_PRIVACY_LEVELS",
+    "MEMORY_SOURCE_TYPES",
+    "MemoryCandidate",
     "MemoryKind",
     "MemoryPrivacy",
+    "MemoryProposalBundle",
+    "MemoryProposalError",
     "MemoryRecord",
     "MemorySource",
     "MemorySourceType",
     "MemoryStore",
+    "ModelMemoryProposer",
     "NewMemory",
 ]
