b34e27d2157d000065c7c6585612ad11db3edf5a
david00andersen <david00andersen@proton.me>
2026-09-18 18:45:26 -0700

    feat: export SQLite knowledge store

--- src/ally/storage/sqlite/__init__.py
+++ src/ally/storage/sqlite/__init__.py
@@ -2,6 +2,12 @@
 
 from ally.storage.sqlite.conversations import SQLiteConversationStore
 from ally.storage.sqlite.database import SQLiteDatabase
+from ally.storage.sqlite.knowledge import SQLiteKnowledgeStore
 from ally.storage.sqlite.memory import SQLiteMemoryStore
 
-__all__ = ["SQLiteConversationStore", "SQLiteDatabase", "SQLiteMemoryStore"]
+__all__ = [
+    "SQLiteConversationStore",
+    "SQLiteDatabase",
+    "SQLiteKnowledgeStore",
+    "SQLiteMemoryStore",
+]
