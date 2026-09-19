ccf6242411cd40b269e4d1033bee2c600503f328
david00andersen <david00andersen@proton.me>
2026-09-18 19:04:22 -0700

    feat: export tool audit store

--- src/ally/storage/sqlite/__init__.py
+++ src/ally/storage/sqlite/__init__.py
@@ -4,10 +4,12 @@ from ally.storage.sqlite.conversations import SQLiteConversationStore
 from ally.storage.sqlite.database import SQLiteDatabase
 from ally.storage.sqlite.knowledge import SQLiteKnowledgeStore
 from ally.storage.sqlite.memory import SQLiteMemoryStore
+from ally.storage.sqlite.tool_audit import SQLiteToolAuditStore
 
 __all__ = [
     "SQLiteConversationStore",
     "SQLiteDatabase",
     "SQLiteKnowledgeStore",
     "SQLiteMemoryStore",
+    "SQLiteToolAuditStore",
 ]
