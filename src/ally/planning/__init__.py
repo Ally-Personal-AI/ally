ed4ddfe22b90d3778f9bb3b408fcd0b614c44bc5
david00andersen <david00andersen@proton.me>
2026-09-18 19:46:49 -0700

    feat: add planning package

---
+++ src/ally/planning/__init__.py
@@ -0,0 +1,8 @@
+"""Non-executing model planning boundary."""
+
+from ally.planning.model_planner import ModelTaskPlanner, PlanProposalError
+
+__all__ = [
+    "ModelTaskPlanner",
+    "PlanProposalError",
+]
