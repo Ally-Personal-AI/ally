7cb327cac08e3fca21db303b66e1ecfaff6ee22e
david00andersen <david00andersen@proton.me>
2026-09-18 23:04:11 -0700

    fix: reconcile skill audit export with current readiness architecture

--- src/ally/storage/sqlite/__init__.py
+++ src/ally/storage/sqlite/__init__.py
@@ -4,7 +4,6 @@ from ally.storage.sqlite.attention import SQLiteAttentionDeliveryStore
 from ally.storage.sqlite.conversations import SQLiteConversationStore
 from ally.storage.sqlite.database import SQLiteDatabase
 from ally.storage.sqlite.events import SQLiteEventStore
-from ally.storage.sqlite.health import build_sqlite_service_health
 from ally.storage.sqlite.knowledge import SQLiteKnowledgeStore
 from ally.storage.sqlite.memory import SQLiteMemoryStore
 from ally.storage.sqlite.schedules import SQLiteScheduleStore
@@ -27,5 +26,4 @@ __all__ = [
     "SQLiteSkillExecutionAuditStore",
     "SQLiteTaskStore",
     "SQLiteToolAuditStore",
-    "build_sqlite_service_health",
 ]
