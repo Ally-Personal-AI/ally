744aeffe49ba4ef2d625a9a7ca8f3ec0683c2dce
david00andersen <david00andersen@proton.me>
2026-09-23 18:12:25 -0700

    Add typed controlled egress domain

---
+++ src/ally/egress/__init__.py
@@ -0,0 +1,32 @@
+"""Controlled external-data egress boundary."""
+
+from ally.egress.adapter import EgressAdapter
+from ally.egress.audit import EgressAuditRecord, EgressAuditStore
+from ally.egress.executor import EgressExecutor
+from ally.egress.models import (
+    EgressDataClass,
+    EgressDecision,
+    EgressExecution,
+    EgressField,
+    EgressFieldManifest,
+    EgressInspection,
+    EgressRequest,
+    EgressStatus,
+)
+from ally.egress.policy import DefaultEgressPolicy
+
+__all__ = [
+    "DefaultEgressPolicy",
+    "EgressAdapter",
+    "EgressAuditRecord",
+    "EgressAuditStore",
+    "EgressDataClass",
+    "EgressDecision",
+    "EgressExecution",
+    "EgressExecutor",
+    "EgressField",
+    "EgressFieldManifest",
+    "EgressInspection",
+    "EgressRequest",
+    "EgressStatus",
+]
