c6ee94cfa01b9ded905bb78834d0a28b77428030
david00andersen <david00andersen@proton.me>
2026-09-25 11:50:02 -0700

    Export local research synthesis boundary

--- src/ally/research/__init__.py
+++ src/ally/research/__init__.py
@@ -4,7 +4,9 @@ from ally.research.models import (
     MAX_RESEARCH_QUERY_CHARS,
     MAX_RESEARCH_QUERY_WORDS,
     MAX_RESEARCH_RESULTS,
+    WebResearchAnswerExecution,
     WebResearchExecution,
+    WebResearchSynthesis,
     WebSearchPayload,
     WebSearchRequest,
     WebSearchResult,
@@ -22,8 +24,14 @@ __all__ = [
     "WEB_SEARCH_OPERATION",
     "ResearchService",
     "ResearchServiceError",
+    "WebResearchAnswerExecution",
     "WebResearchExecution",
+    "WebResearchSynthesis",
+    "WebResearchSynthesizer",
+    "ResearchSynthesisError",
     "WebSearchPayload",
     "WebSearchRequest",
     "WebSearchResult",
 ]
+
+from ally.research.synthesis import ResearchSynthesisError, WebResearchSynthesizer
