f2c4757fd05bac2227109e8bed9d562623a5a141
david00andersen <david00andersen@proton.me>
2026-09-25 10:47:07 -0700

    Replay settings work: desktop/macos/Sources/AllyDesktopCore/BridgeClient.swift

--- desktop/macos/Sources/AllyDesktopCore/BridgeClient.swift
+++ desktop/macos/Sources/AllyDesktopCore/BridgeClient.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct DesktopBridgeClient: Sendable {
     public static let helperEnvironmentKey = "ALLY_DESKTOP_BRIDGE"
     public static let maximumResponseBytes = 2 * 1024 * 1024
-    public static let supportedProtocolVersion = 8
+    public static let supportedProtocolVersion = 9
 
     public let executableURL: URL
 
